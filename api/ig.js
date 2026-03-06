0274ddc80eea3bb9c6564d6881247c5742f86895
MageZen <princemaurya103@gmail.com>
2026-03-06 17:52:49 +0530

    Update ig.js

--- api/ig.js
+++ api/ig.js
@@ -1,75 +1,48 @@
 const axios = require("axios")
-const cheerio = require("cheerio")
 
-module.exports = async function (req, res) {
+module.exports = async function(req, res){
 
-  try {
+  try{
 
     const { url } = req.query
 
-    if (!url) {
+    if(!url){
       return res.json({
         status:false,
         message:"Missing URL"
       })
     }
 
-    // convert instagram -> ddinstagram
-    const newUrl = url.replace(
-      "instagram.com",
-      "ddinstagram.com"
-    )
+    // convert instagram link
+    const apiUrl =
+      "https://ddinstagram.com" +
+      url.split("instagram.com")[1]
 
-    const response = await axios.get(newUrl,{
+    const response = await axios.get(apiUrl,{
       headers:{
         "User-Agent":"Mozilla/5.0"
       }
     })
 
-    const html = response.data
-    const $ = cheerio.load(html)
-
-    const video =
-      $("meta[property='og:video']").attr("content")
-
-    const image =
-      $("meta[property='og:image']").attr("content")
-
-    let media=[]
-
-    if(video){
-      media.push({
-        type:"video",
-        url:video
-      })
-    }
-
-    if(image){
-      media.push({
-        type:"image",
-        url:image
-      })
-    }
-
-    if(media.length===0){
-
-      return res.json({
-        status:false,
-        message:"Media not found"
-      })
-
-    }
+    // final redirected url
+    const finalUrl =
+      response.request.res.responseUrl
 
     res.json({
       status:true,
-      media:media
+      media:[
+        {
+          type:"video",
+          url:finalUrl
+        }
+      ]
     })
 
-  } catch(e){
+  }catch(e){
 
     res.json({
       status:false,
-      message:"Server error"
+      message:e.message
     })
 
   }
